20ca327bc57676ff643bdb972663232e5145dfdb
RuaaAlsugheir <alsugheirruaa@gmail.com>
2022-05-02 10:34:28 +0300

    Home page rough build

--- Task-3/src/Home/Home.js
+++ Task-3/src/Home/Home.js
@@ -1,12 +1,23 @@
 import React from "react";
 import NavBar from "../Styled Elements/NavBar/NavBar";
+import Container from "../Styled Elements/Container/Container";
+import Footer from "../Styled Elements/Footer/Footer";
 import "./Home.css";
 
 export default function Home() {
   return (
     <div className="home">
       <NavBar />
-      Home
+      <Container type="whole-page home-page">
+        <Container type="form">
+          Cards
+        </Container>
+        <aside>
+          <section className="accounts"></section>
+          <section className="suggestions"></section>
+          <Footer />
+        </aside>
+      </Container>
     </div>
   );
 }
